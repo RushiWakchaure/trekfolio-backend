882971b2b7e2a4f842dfa5dfc1fc04e105e78c04
Your Name <your.email@example.com>
2026-07-21 20:22:43 +0530

    Fix SecurityConfig and UserController

--- src/main/java/com/trekfolio/controller/UserController.java
+++ src/main/java/com/trekfolio/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.trekfolio.controller;
 
+import com.trekfolio.dto.LoginRequest;
+import com.trekfolio.dto.LoginResponse;
 import com.trekfolio.entity.User;
 import com.trekfolio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.trekfolio.dto.LoginResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import com.trekfolio.dto.LoginRequest;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,33 +18,26 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public User register(@RequestBody User user){
-
+    public User register(@RequestBody User user) {
         return userService.registerUser(user);
+    }
 
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return userService.loginUser(
+                request.getEmail(),
+                request.getPassword()
+        );
     }
 
     @GetMapping
-    public List<User> getUsers(){
-
+    public List<User> getUsers() {
         return userService.getAllUsers();
-
     }
-    @PostMapping("/login")
-    public LoginResponse login(@RequestBody LoginRequest request){
 
-        return userService.loginUser(
-                request.getEmail(),
-                request.getPassword());
-
-    }
     @GetMapping("/me")
     public User getCurrentUser(Authentication authentication) {
-
         String email = authentication.getName();
-
         return userService.getUserByEmail(email);
-
     }
-
 }
--- src/main/java/com/trekfolio/security/SecurityConfig.java
+++ src/main/java/com/trekfolio/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/users/register", "/api/users/login").permitAll()
                         .anyRequest().authenticated()
-                )
+                );
                 //.addFilterBefore(jwtAuthenticationFilter,
                        // UsernamePasswordAuthenticationFilter.class);
 
